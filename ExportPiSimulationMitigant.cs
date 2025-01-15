eca850730ce6a90e5f182e9b48222782a29dcbf4
Steven Liu - Personal <51730159+Steven-Liu-0914@users.noreply.github.com>
2025-01-15 14:19:32 +0800

    Add files via upload

---
+++ ExportPiSimulationMitigant.cs
@@ -0,0 +1,21 @@
+﻿using CACIB.CREW.Api.Infrastructure;
+
+namespace CACIB.CREW.Api.Features.GeneralExport.Model
+{
+    public class ExportPiSimulationMitigant : BaseResponse
+    {
+        public new IList<ExportPiSimulationMitigantItem>? Data { get; set; }
+    }
+
+    public class ExportPiSimulationMitigantItem
+    {
+        public int MitigantId { get; set; }
+        public string? Type { get; set; }
+        public decimal? Version { get; set; }
+        public string? AssignmentLevel { get; set; }
+        public string? GuarantorOrDepositoryName { get; set; }
+        public decimal? Amount { get; set; }
+        public string? Currency { get; set; }
+        public string? Status { get; set; }
+    }
+}
---
+++ ExportPiSimulationSchedule.cs
@@ -0,0 +1,23 @@
+﻿using CACIB.CREW.Api.Infrastructure;
+
+namespace CACIB.CREW.Api.Features.GeneralExport.Model
+{
+    public class ExportPiSimulationSchedule : BaseResponse
+    {
+        public new IList<ExportPiSimulationScheduleItem>? Data { get; set; }
+    }
+
+
+    public class ExportPiSimulationScheduleItem
+    {
+        public DateTime? DueDate { get; set; }
+        public decimal? DueAmount { get; set; }
+        public decimal? AuthorizedAmount { get; set; }
+        public string? Type { get; set; }
+        public string? AuthorizedAmountCurrency { get; set; }
+        public decimal? UtilizationForecast { get; set; }
+        public decimal? Margin { get; set; }
+        public decimal? PercentageOfUse { get; set; }
+        public decimal? CommitmentFees { get; set; }
+    }
+}
